aaa06f1bda680392683e7f4c9d70e9ba73680975
Owen Hovey <247624724+ohovey-pfizer@users.noreply.github.com>
2026-02-24 11:59:46 -0500

    Added config files
    Added support for YAML configs.
    
    Configs now defined in configs folder, where user can dynamically update parameters of each class.
    
    Updated main.py to reflect changes.
    
    Added base_config as default params.

---
+++ src/config_loader.py
@@ -0,0 +1,39 @@
+import yaml
+
+def load_config(config_path='configs/config_base.yaml'):
+    """Load and merge YAML configs.
+    
+    Defaults to values in 'config_base.yaml', overrides if there are any overrides needed
+    """
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+    
+    return config
+
+# Helper function to combine forecasts, actuals, and accrual data into one JSON formatted dictionary
+def combine_data(forecast, transactional):
+    combined = {}
+
+    # All PO numbers that exist in either dataset
+    all_pos = set(forecast.keys()) | set(transactional.keys())
+
+    months_list = [
+        "Jan","Feb","Mar","Apr","May","Jun",
+        "Jul","Aug","Sep","Oct","Nov","Dec"
+    ]
+
+    for po in all_pos:
+        combined[po] = {}
+
+        for month in months_list:
+            f = forecast.get(po, {}).get(month, {})
+            t = transactional.get(po, {}).get(month, {})
+
+            combined[po][month] = {
+                "Forecast": f.get("Forecast", 0),
+                "Actual": t.get("Actual", 0),
+                "Accrual": t.get("Accrual", 0),
+                "Accrual Reversal": t.get("Reversal", 0)
+            }
+
+    return combined
